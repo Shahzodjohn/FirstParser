67083d7ae0067d55016c1eca3e50e65b43e373d1
Shahzod <Shahzod.akhmedov@mail.ru>
2021-08-05 16:38:35 +0500

    Images

--- r/Controllers/ValuesController.cs
+++ r/Controllers/ValuesController.cs
@@ -6,6 +6,7 @@ using r.Entities;
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -38,8 +39,6 @@ namespace r.Controllers
             request.AddHeader("Cookie", "XSRF-TOKEN=eyJpdiI6IkZmdGk4SFJ0cUV6b08rZkxOa1ZwbWc9PSIsInZhbHVlIjoibU5yXC9ka2tka0xiVzVuWHdDVktPSDhoMUNhM2xHdG9HNWxrakZLOU5vQ3cwNklFOHZwT3k1Yk1XNmlZOUQwdysiLCJtYWMiOiJlZWM3YjY1NjQ1Yjg0NmVmYTkxNGFkYzdjNThhNmE4NmQ1YmMzMGJiMGY3ZGYzMjNjYmFhNjdkYTZkYzMxZmEwIn0%3D; e572d25e2f77b01121404f0eef127450=6c59d20d6edf5cab42e47435612d8098; laravel_session=eyJpdiI6Ik5CS0NMNzAweGY3aFdtWlRZbEFLOFE9PSIsInZhbHVlIjoiZFBvT0c4WUdSYTh1NUE5M3RUTjFCWDFpaDlkbDk3Q0tBZUViSnBEdFE1SVltMGh1ZDdHdVgyelBCa3ZKYkFpUiIsIm1hYyI6IjgxMjk2MzYwNjI2NGY2NDUxMDgwOGUxYWFkYTE2YTcyNzQ1Yjk1M2E3MDQ4NDQ3ZDhhYzY3MmIzZmE4MTlhNjYifQ%3D%3D");
             IRestResponse response = GetClient.Execute(request);
             
-
-
 
             var PostClient = new RestClient("https://mojegs1.pl/logowanie/zaloguj-uzytkownika");
             PostClient.Timeout = -1;
@@ -166,9 +165,26 @@ namespace r.Controllers
                         var rr = (isClassicCategory ? classicCategory.Id :
                                 (isFasterCategory ? fasterCategory.Id : nocategory.Id));
 
-
-                       
-
+                        var gting = new List<string>();
+                        gting.Add(items[3].InnerText.Trim());
+                        foreach (var gtinImages in gting)
+                        {
+                            using (HttpClient cl = new HttpClient())
+                            {
+                                cl.DefaultRequestHeaders.Add("Cookie", "XSRF-TOKEN=eyJpdiI6ImdVZnBTa25xekswUlNsXC9FcTI5TmRBPT0iLCJ2YWx1ZSI6ImR5K1hQcWNwVk9vRlRnSDJkTW1US25MQWszVjNyY1R6SFwvUGJwOVFmVXZya090aDZsQ2I5QnhHRUFPTE9GdzI2IiwibWFjIjoiMTcxNzExMGI0MTI3NmU0NDliMGUwZTYwNDhjYTA3MDc5Zjk3Y2EzN2VjZWVjYzRhNWQyYTBkNDU3NjI2OTMzNyJ9; e572d25e2f77b01121404f0eef127450=6c59d20d6edf5cab42e47435612d8098; laravel_session=eyJpdiI6InFhZHF2OUN1Q3g3NmE5cWlNbVZ1K0E9PSIsInZhbHVlIjoiTjFLYldvVExzcXlqbWxYRFNUaERZc0o1R280cGhSQ2ROWVpjZlRNOEcyVjdxMGY4Z1JvdnREQmhjR3N0VHRjQSIsIm1hYyI6ImEzMjUwMGE4ZTRmNTFiN2ExYzA2YTY4ZGYxOTQwYzdjN2Y0ZDAxNmMyOWQ5NjczNmY2ZjQ4OGY2OWQzZDVhZjMifQ%3D%3D"
+                                );
+                                string url = ($"https://mojegs1.pl/moje-produkty/pobierz-etykiete?gtin={gtinImages}&label_text=ekoTuptusie+APLIKACJA+PAN+SAMOCHODZIK+CLASSIC+rozm.+32&size=1&extension=png");
+                                using (HttpResponseMessage MessageResponse = await cl.GetAsync(url))
+                                using (Stream streamToReadFrom = await MessageResponse.Content.ReadAsStreamAsync())
+                                {
+                                    string fileToWriteTo = Path.GetFullPath($"PdfCodes/{gtinImages}.png");
+                                    using (Stream streamToWriteTo = System.IO.File.Open(fileToWriteTo, FileMode.Create))
+                                    {
+                                        await streamToReadFrom.CopyToAsync(streamToWriteTo);
+                                    }
+                                }
+                            }
+                        }
                         _context.DRTS.Add(new DRT
                         {
                             NAZWAPRODUKTU = output,
