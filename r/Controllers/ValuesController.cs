afdff8b1d5db65a5db9193f4760d776e59fb1930
Shahzod <Shahzod.akhmedov@mail.ru>
2021-08-09 14:36:13 +0500

    AddingCookie

--- r/Controllers/ValuesController.cs
+++ r/Controllers/ValuesController.cs
@@ -8,11 +8,13 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace r.Controllers
 {
@@ -32,30 +34,41 @@ namespace r.Controllers
         {
             using var client = new HttpClient();
             //client.DefaultRequestHeaders.Add("Cookie", "ElS4TqDdrI5g96loAR61laSN0llwrbQkvLAXuzba");
-
-            var GetClient = new RestClient("https://mojegs1.pl/logowanie");
-            GetClient.Timeout = -1;
-            var request = new RestRequest(Method.GET);
-            request.AddHeader("Cookie", "XSRF-TOKEN=eyJpdiI6IkZmdGk4SFJ0cUV6b08rZkxOa1ZwbWc9PSIsInZhbHVlIjoibU5yXC9ka2tka0xiVzVuWHdDVktPSDhoMUNhM2xHdG9HNWxrakZLOU5vQ3cwNklFOHZwT3k1Yk1XNmlZOUQwdysiLCJtYWMiOiJlZWM3YjY1NjQ1Yjg0NmVmYTkxNGFkYzdjNThhNmE4NmQ1YmMzMGJiMGY3ZGYzMjNjYmFhNjdkYTZkYzMxZmEwIn0%3D; e572d25e2f77b01121404f0eef127450=6c59d20d6edf5cab42e47435612d8098; laravel_session=eyJpdiI6Ik5CS0NMNzAweGY3aFdtWlRZbEFLOFE9PSIsInZhbHVlIjoiZFBvT0c4WUdSYTh1NUE5M3RUTjFCWDFpaDlkbDk3Q0tBZUViSnBEdFE1SVltMGh1ZDdHdVgyelBCa3ZKYkFpUiIsIm1hYyI6IjgxMjk2MzYwNjI2NGY2NDUxMDgwOGUxYWFkYTE2YTcyNzQ1Yjk1M2E3MDQ4NDQ3ZDhhYzY3MmIzZmE4MTlhNjYifQ%3D%3D");
-            IRestResponse response = GetClient.Execute(request);
-            
+            HtmlAgilityPack.HtmlDocument web = new HtmlAgilityPack.HtmlDocument();
+            //var GetClient = new RestClient("https://mojegs1.pl/logowanie");
+            //GetClient.Timeout = -1;
+            //var request = new RestRequest(Method.GET);
+            //request.AddHeader("Cookie", "XSRF-TOKEN=eyJpdiI6IlNpQlhpaEl6MlFHMTF1YkZET1hWS3c9PSIsInZhbHVlIjoiektJalBWK2VlZHo2UVJlcm1MaDRhNjJwVGhVUFVmOGM4MDI0eU5YRGVqV2dwUjJDZHNTSWpURERBR0FPVXF4ViIsIm1hYyI6IjI4YWY2NWRlMTRhOGZmZGY5NTU1ZGViYTJmOThmNzQ5YjNkMzI0Y2M0ZDNlYjdjYTI2ZjQ2Yzk1MGM2YWNiMzgifQ%3D%3D; e572d25e2f77b01121404f0eef127450=7af44baa71dff9a37418e0d617d510fa; laravel_session=eyJpdiI6Imo0aWxNRFI1aktBenBneDdtUE5JQnc9PSIsInZhbHVlIjoiRHp5MmY1cEpHRHczU21jXC9KanZQMnkyUnVUa2RhNUNnTmJjS1J2cDRpVVpnbU1zQUxzVWNcLzN0UElBXC9uWThkaSIsIm1hYyI6IjcyMTgxMTIyZTU3MGEyMDY3MTc2Y2U4OWMyM2NkYWYyMDQ1OWMyNGFjYjY4MDkwM2NmNzY5NGE0ZTZmZDY1ODUifQ%3D%3D");
+            //IRestResponse response = GetClient.Execute(request);
+            //web.LoadHtml(response.Content);
 
             var PostClient = new RestClient("https://mojegs1.pl/logowanie/zaloguj-uzytkownika");
             PostClient.Timeout = -1;
             var PostRequest = new RestRequest(Method.POST);
             PostRequest.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-            PostRequest.AddHeader("Cookie", "XSRF-TOKEN=eyJpdiI6ImdVZnBTa25xekswUlNsXC9FcTI5TmRBPT0iLCJ2YWx1ZSI6ImR5K1hQcWNwVk9vRlRnSDJkTW1US25MQWszVjNyY1R6SFwvUGJwOVFmVXZya090aDZsQ2I5QnhHRUFPTE9GdzI2IiwibWFjIjoiMTcxNzExMGI0MTI3NmU0NDliMGUwZTYwNDhjYTA3MDc5Zjk3Y2EzN2VjZWVjYzRhNWQyYTBkNDU3NjI2OTMzNyJ9; e572d25e2f77b01121404f0eef127450=6c59d20d6edf5cab42e47435612d8098; laravel_session=eyJpdiI6InFhZHF2OUN1Q3g3NmE5cWlNbVZ1K0E9PSIsInZhbHVlIjoiTjFLYldvVExzcXlqbWxYRFNUaERZc0o1R280cGhSQ2ROWVpjZlRNOEcyVjdxMGY4Z1JvdnREQmhjR3N0VHRjQSIsIm1hYyI6ImEzMjUwMGE4ZTRmNTFiN2ExYzA2YTY4ZGYxOTQwYzdjN2Y0ZDAxNmMyOWQ5NjczNmY2ZjQ4OGY2OWQzZDVhZjMifQ%3D%3D");
+            PostRequest.AddHeader("Cookie", "XSRF-TOKEN=eyJpdiI6IjNtbUp4OE51aXU2Yjk1Rk14MkNSMGc9PSIsInZhbHVlIjoiMm9tVU96ZlBLOWZXcEpKY040UXhid1ZHRzBTeVBMNHZYVUpoaCt5MjE5alVDOVVBSWphK0RoN1BHMFNaRmRQciIsIm1hYyI6IjZiNDFkYzEzZDIwNDE5NzhjZmJhNmQ0MGU2YmZkODQzZDJlODZiY2QzNjZmZTY1YjI3Mjg1Nzg3N2Y4YjVkNGEifQ%3D%3D; e572d25e2f77b01121404f0eef127450=7af44baa71dff9a37418e0d617d510fa; laravel_session=eyJpdiI6ImtXdVhmekJnZVZyNXVKeTZvc3VzeWc9PSIsInZhbHVlIjoiQXNES1RXS216Nmw1S2R4OGUxaTBEcXYzQ3VhcCtWcFd4ZVlhNUlHMERzRlJscWNQaGdKTXByTWw4eTZIM01rcCIsIm1hYyI6IjFhYzMyYjQ0NDNkOTFjN2Q2ZDI4NDUzYzQyZmNkYTAzY2JjODQxYWEwN2M0NWY4ZTE2OTFhZmEwYmFmNWM1NjEifQ%3D%3D");
             PostRequest.AddParameter("email", "tomek@fiorino.eu");
             PostRequest.AddParameter("password", "Epoka1-wsx");
             IRestResponse PostResponse = PostClient.Execute(PostRequest);
+            web.LoadHtml(PostResponse.Content);
+
+            var value = "";
+            var cookieFirst = PostResponse.Cookies[0].Value;
+            var cookieSecond = PostResponse.Cookies[1].Value;
+            //var count2 = count.
+            
 
 
 
             var RstClient = new RestClient("https://mojegs1.pl/moje-produkty/sortowanie/nazwa/kierunek/rosnaco/1?searchText=&isPublic=&amountPerPage=1000");
             RstClient.Timeout = -1;
             var RestRequest = new RestRequest(Method.GET);
-            RestRequest.AddHeader("Cookie", "XSRF-TOKEN=eyJpdiI6ImdVZnBTa25xekswUlNsXC9FcTI5TmRBPT0iLCJ2YWx1ZSI6ImR5K1hQcWNwVk9vRlRnSDJkTW1US25MQWszVjNyY1R6SFwvUGJwOVFmVXZya090aDZsQ2I5QnhHRUFPTE9GdzI2IiwibWFjIjoiMTcxNzExMGI0MTI3NmU0NDliMGUwZTYwNDhjYTA3MDc5Zjk3Y2EzN2VjZWVjYzRhNWQyYTBkNDU3NjI2OTMzNyJ9; e572d25e2f77b01121404f0eef127450=6c59d20d6edf5cab42e47435612d8098; laravel_session=eyJpdiI6InFhZHF2OUN1Q3g3NmE5cWlNbVZ1K0E9PSIsInZhbHVlIjoiTjFLYldvVExzcXlqbWxYRFNUaERZc0o1R280cGhSQ2ROWVpjZlRNOEcyVjdxMGY4Z1JvdnREQmhjR3N0VHRjQSIsIm1hYyI6ImEzMjUwMGE4ZTRmNTFiN2ExYzA2YTY4ZGYxOTQwYzdjN2Y0ZDAxNmMyOWQ5NjczNmY2ZjQ4OGY2OWQzZDVhZjMifQ%3D%3D");
+            RestRequest.AddHeader("Cookie", "XSRF-TOKEN=eyJpdiI6IjNtbUp4OE51aXU2Yjk1Rk14MkNSMGc9PSIsInZhbHVlIjoiMm9tVU96ZlBLOWZXcEpKY040UXhid1ZHRzBTeVBMNHZYVUpoaCt5MjE5alVDOVVBSWphK0RoN1BHMFNaRmRQciIsIm1hYyI6IjZiNDFkYzEzZDIwNDE5NzhjZmJhNmQ0MGU2YmZkODQzZDJlODZiY2QzNjZmZTY1YjI3Mjg1Nzg3N2Y4YjVkNGEifQ%3D%3D; e572d25e2f77b01121404f0eef127450=7af44baa71dff9a37418e0d617d510fa; laravel_session=eyJpdiI6ImtXdVhmekJnZVZyNXVKeTZvc3VzeWc9PSIsInZhbHVlIjoiQXNES1RXS216Nmw1S2R4OGUxaTBEcXYzQ3VhcCtWcFd4ZVlhNUlHMERzRlJscWNQaGdKTXByTWw4eTZIM01rcCIsIm1hYyI6IjFhYzMyYjQ0NDNkOTFjN2Q2ZDI4NDUzYzQyZmNkYTAzY2JjODQxYWEwN2M0NWY4ZTE2OTFhZmEwYmFmNWM1NjEifQ%3D%3D");
+            RestRequest.AddHeader("Cookie", cookieFirst);
+            RestRequest.AddHeader("Cookie", cookieSecond);
             IRestResponse RstResponse = RstClient.Execute(RestRequest);
+            web.LoadHtml(PostResponse.Content);
+
 
 
             var linkCount = 1;
@@ -65,12 +78,12 @@ namespace r.Controllers
                 var LinkClient = new RestClient($"https://mojegs1.pl/moje-produkty/{linkCount}");
                 LinkClient.Timeout = -1;
                 var LinkRequest = new RestRequest(Method.GET);
-                LinkRequest.AddHeader("Cookie", "XSRF-TOKEN=eyJpdiI6ImdVZnBTa25xekswUlNsXC9FcTI5TmRBPT0iLCJ2YWx1ZSI6ImR5K1hQcWNwVk9vRlRnSDJkTW1US25MQWszVjNyY1R6SFwvUGJwOVFmVXZya090aDZsQ2I5QnhHRUFPTE9GdzI2IiwibWFjIjoiMTcxNzExMGI0MTI3NmU0NDliMGUwZTYwNDhjYTA3MDc5Zjk3Y2EzN2VjZWVjYzRhNWQyYTBkNDU3NjI2OTMzNyJ9; e572d25e2f77b01121404f0eef127450=6c59d20d6edf5cab42e47435612d8098; laravel_session=eyJpdiI6InFhZHF2OUN1Q3g3NmE5cWlNbVZ1K0E9PSIsInZhbHVlIjoiTjFLYldvVExzcXlqbWxYRFNUaERZc0o1R280cGhSQ2ROWVpjZlRNOEcyVjdxMGY4Z1JvdnREQmhjR3N0VHRjQSIsIm1hYyI6ImEzMjUwMGE4ZTRmNTFiN2ExYzA2YTY4ZGYxOTQwYzdjN2Y0ZDAxNmMyOWQ5NjczNmY2ZjQ4OGY2OWQzZDVhZjMifQ%3D%3D");
+                LinkRequest.AddHeader("Cookie", "XSRF-TOKEN=eyJpdiI6IjNtbUp4OE51aXU2Yjk1Rk14MkNSMGc9PSIsInZhbHVlIjoiMm9tVU96ZlBLOWZXcEpKY040UXhid1ZHRzBTeVBMNHZYVUpoaCt5MjE5alVDOVVBSWphK0RoN1BHMFNaRmRQciIsIm1hYyI6IjZiNDFkYzEzZDIwNDE5NzhjZmJhNmQ0MGU2YmZkODQzZDJlODZiY2QzNjZmZTY1YjI3Mjg1Nzg3N2Y4YjVkNGEifQ%3D%3D; e572d25e2f77b01121404f0eef127450=7af44baa71dff9a37418e0d617d510fa; laravel_session=eyJpdiI6ImtXdVhmekJnZVZyNXVKeTZvc3VzeWc9PSIsInZhbHVlIjoiQXNES1RXS216Nmw1S2R4OGUxaTBEcXYzQ3VhcCtWcFd4ZVlhNUlHMERzRlJscWNQaGdKTXByTWw4eTZIM01rcCIsIm1hYyI6IjFhYzMyYjQ0NDNkOTFjN2Q2ZDI4NDUzYzQyZmNkYTAzY2JjODQxYWEwN2M0NWY4ZTE2OTFhZmEwYmFmNWM1NjEifQ%3D%3D");
                 IRestResponse LinkResponse = LinkClient.Execute(LinkRequest);
                 linkCount++;
                 
                 
-                HtmlAgilityPack.HtmlDocument web = new HtmlAgilityPack.HtmlDocument();
+                
                 web.LoadHtml(LinkResponse.Content);
 
                 var xpath = "//tr";
@@ -125,11 +138,10 @@ namespace r.Controllers
                         var isFasterCategory = items[1].InnerText.Trim().Split(" ").ToList().Any(x => x.ToLower() == "faster");
 
 
-
-
                         var productName = isClassicCategory ? items[1].InnerText.Trim().Replace("CLASSIC", " ") :
                                           isFasterCategory ? items[1].InnerText.Trim().Replace("FASTER", " ") :
                                           items[1].InnerText.Trim();
+                        var PorductFullName = items[1].InnerText.Trim();
                         //var contains = productName.Contains("rozm.");
 
                         #region Deviding by regex
@@ -156,7 +168,7 @@ namespace r.Controllers
                         //productName = productName.Replace("rozm.", " ");
                         //var output = Regex.Replace(productName, @"[\0-9]", " ");
                         #endregion
-
+                        
                         // Regex.Replace(productName, "[^0-9]", "");
                         var ProdName = productName.Replace("rozm.", " ");
                         var output = Regex.Replace(ProdName, @"[\0-9]", " ");
@@ -165,15 +177,23 @@ namespace r.Controllers
                         var rr = (isClassicCategory ? classicCategory.Id :
                                 (isFasterCategory ? fasterCategory.Id : nocategory.Id));
 
+                        
+                        var nazvaProductu = new List<string>();
+                        nazvaProductu.Add(PorductFullName);
+
+
                         var gting = new List<string>();
                         gting.Add(items[3].InnerText.Trim());
+
+
                         foreach (var gtinImages in gting)
                         {
                             using (HttpClient cl = new HttpClient())
                             {
                                 cl.DefaultRequestHeaders.Add("Cookie", "XSRF-TOKEN=eyJpdiI6ImdVZnBTa25xekswUlNsXC9FcTI5TmRBPT0iLCJ2YWx1ZSI6ImR5K1hQcWNwVk9vRlRnSDJkTW1US25MQWszVjNyY1R6SFwvUGJwOVFmVXZya090aDZsQ2I5QnhHRUFPTE9GdzI2IiwibWFjIjoiMTcxNzExMGI0MTI3NmU0NDliMGUwZTYwNDhjYTA3MDc5Zjk3Y2EzN2VjZWVjYzRhNWQyYTBkNDU3NjI2OTMzNyJ9; e572d25e2f77b01121404f0eef127450=6c59d20d6edf5cab42e47435612d8098; laravel_session=eyJpdiI6InFhZHF2OUN1Q3g3NmE5cWlNbVZ1K0E9PSIsInZhbHVlIjoiTjFLYldvVExzcXlqbWxYRFNUaERZc0o1R280cGhSQ2ROWVpjZlRNOEcyVjdxMGY4Z1JvdnREQmhjR3N0VHRjQSIsIm1hYyI6ImEzMjUwMGE4ZTRmNTFiN2ExYzA2YTY4ZGYxOTQwYzdjN2Y0ZDAxNmMyOWQ5NjczNmY2ZjQ4OGY2OWQzZDVhZjMifQ%3D%3D"
                                 );
-                                string url = ($"https://mojegs1.pl/moje-produkty/pobierz-etykiete?gtin={gtinImages}&label_text=ekoTuptusie+APLIKACJA+PAN+SAMOCHODZIK+CLASSIC+rozm.+32&size=1&extension=png");
+                                string url = $"https://mojegs1.pl/moje-produkty/pobierz-etykiete?gtin={gtinImages}&label_text={PorductFullName}&size=1&extension=png";
+                               // string url = $"https://mojegs1.pl/moje-produkty/pobierz-etykiete?gtin={gtinImages}&label_text=ekoTuptusie+APLIKACJA+B%C5%81%C4%98KITNE+%C5%81APKI+NA+SZARYM+CLASSIC+rozm.40&size=1&extension=png";
                                 using (HttpResponseMessage MessageResponse = await cl.GetAsync(url))
                                 using (Stream streamToReadFrom = await MessageResponse.Content.ReadAsStreamAsync())
                                 {
@@ -198,7 +218,6 @@ namespace r.Controllers
                 }
                 catch (Exception)
                 {
-
                     break;
                 }
                 await _context.SaveChangesAsync();
